e896814e9a825be3395835fdbe2bfcddb433b6ee
Helen Hou-Sandi <helen.y.hou@gmail.com>
2014-09-13 14:45:45 -0400

    Add the profile option and save it.

--- simple-new-post-emails.php
+++ simple-new-post-emails.php
@@ -33,6 +33,8 @@ class Simple_New_Post_Emails {
 	public function __construct() {
 		add_action( 'widgets_init', array( $this, 'widgets_init' ) );
 		add_action( 'personal_options', array( $this, 'personal_options' ) );
+		add_action( 'edit_user_profile_update', array( $this, 'personal_options_save' ) );
+		add_action( 'personal_options_update', array( $this, 'personal_options_save' ) );
 		add_action( 'publish_post', array( $this, 'publish_post' ), 10, 2 );
 	}
 
@@ -49,6 +51,33 @@ class Simple_New_Post_Emails {
 	 * @return null
 	 */
 	public function personal_options( $user ) {
+?>
+<tr class="snpe-option">
+	<th scope="row">New Post Emails</th>
+	<td>
+		<label for="snpe_send">
+		<input name="snpe_send" type="checkbox" id="snpe_send" value="Y"<?php checked( $user->snpe_send, 'Y' ); ?> />
+		<?php _e( 'Send an email when a new post is published' ); ?></label><br />
+	</td>
+</tr>
+<?php
+	}
+
+	/**
+	 * Save our custom profile option.
+	 * @param  int $user_id User ID
+	 * @return null
+	 */
+	public function personal_options_save( $user_id ) {
+		if ( ! current_user_can( 'edit_user', $user_id ) ) {
+			return;
+		}
+
+		if ( isset( $_POST['snpe_send'] ) && 'Y' === $_POST['snpe_send'] ) {
+			update_usermeta( $user_id, 'snpe_send', 'Y' );
+		} else {
+			delete_usermeta( $user_id, 'snpe_send' );
+		}
 	}
 
 	/**
